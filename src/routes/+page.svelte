b397c82286a5b44f8df8ae11ed97aae1791d5b76
Jake <jakeviado434@gmail.com>
2023-08-18 12:18:46 +0800

    kewl transitions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,21 @@
 <script>
+	import { onMount } from 'svelte';
+	import { typewriter } from './transition.js';
+	import { messages } from './loading-messages.js';
+
+	let i = -1;
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			i += 1;
+			i %= messages.length;
+		}, 2500);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+
 </script>
 
 <svelte:head>
@@ -6,9 +23,13 @@
 	<meta name="description" content="Website Portfolio" />
 </svelte:head>
 
-<section class="section-contents">
+
+<section  class="section-contents">
 	<h2 class="big-header">Welcome</h2>
-	<p class="paragraph-contents">for future purposes siguro or mema na lang</p>
+
+	
+	<p  class="paragraph-contents">for future purposes siguro or mema na lang</p>
+	
 	
 	  
 	<article class="card-section1">
@@ -17,7 +38,13 @@
 	  </header>
 	</article>
 
-	
+	<span>
+	{#key i}
+	<p in:typewriter={{ speed: 10 }}>
+		{messages[i] || ''}
+	</p>
+	{/key}
+    </span>
 	
 </section>
 
@@ -25,13 +52,15 @@
  <section class="section-contents section-2" >
 	<h2 class="big-header">i heckin' love programming</h2>
 	<p class="paragraph-contents">i really hecking love programming and designing and i want work and money hehe</p>
-	
+
 	<card class="card-list">
 
 		<article class="card" id="csharp-card">
 		  <header class="card-header">
-			<p>Game Development</p>
-			<h2>cool</h2>
+			
+			<p >Game Development</p>
+			<h2 >cool</h2>
+			
 		  </header>
   
 		  <div class="card-author">
@@ -43,14 +72,16 @@
 			  <div class="author-name-prefix">C#</div>
 			</div>
 		  </div>
-		  <div class="tags">
-		  </div>
 		</article>
+		
 
+		
 		<article class="card" id="js-card">
 		  <header class="card-header">
+			
 			<p>Web Development</p>
-			<h2>wala akong masabi</h2>
+			<h2 >wala akong masabi</h2>
+
 		  </header>
   
 		  <div class="card-author">
@@ -59,10 +90,8 @@
 			</div>
   
 			<div class="author-name">
-			  <div class="author-name-prefix">JavaScript</div>
+			  <div  class="author-name-prefix">JavaScript</div>
 			</div>
-		  </div>
-		  <div class="tags">
 		  </div>
 		</article>
 		
@@ -74,17 +103,15 @@
 			<h2>niced</h2>
 		  </header>
   
-		  <div class="card-author">
-			<div class="avatar">
-				<img src="src/lib/images/html-icon.svg" alt="svelte-icon">
-			</div>
+		  	<div class="card-author">
+				<div class="avatar">
+					<img src="src/lib/images/html-icon.svg" alt="svelte-icon">
+				</div>
   
-			<div class="author-name">
-			  <div class="author-name-prefix">HTML</div>
-			</div>
-		  </div>
-		  <div class="tags">
-		  </div>
+				<div class="author-name">
+			  	<div class="author-name-prefix">HTML</div>
+				</div>
+		  	</div>
 		</article>
 
 		<article class="card">
@@ -102,8 +129,6 @@
 			  <div class="author-name-prefix">CSS</div>
 			</div>
 		  
-		  <div class="tags">
-		  </div>
 		</article>
 
 		<article class="card">
@@ -120,9 +145,7 @@
 			<div class="author-name">
 			  <div class="author-name-prefix">Svelte Kit</div>
 			</div>
-		  
-		  <div class="tags">
-		  </div>
+
 		</article>
 
 	</card>
@@ -181,20 +204,23 @@
 	</card>
 
   </section>
-
+  
 
 
 
 
 <section class="section-contents">
 	<h2 class="big-header">Header 3</h2>
+
 	<p class="paragraph-contents">Debitis doloribus quas omnis quis blanditiis a magni facere laudantium obcaecati praesentium vitae repellat libero incidunt ad voluptates odio hic dolore ea, voluptatum nam! Facilis laborum placeat eligendi facere alias?</p>
+
 </section>
 
 
 <section class="section-contents">
 	<h2 class="big-header">Header 4</h2>
-	<p class="paragraph-contents">Aliquam beatae quis suscipit totam alias quo, neque corporis esse praesentium architecto deserunt ipsum quas velit ut earum, harum est aliquid fuga debitis error aperiam. Voluptatibus repudiandae quis similique. Eum.</p>
+
+	<p class="paragraph-contents">Debitis doloribus quas omnis quis blanditiis a magni facere laudantium obcaecati praesentium vitae repellat libero incidunt ad voluptates odio hic dolore ea, voluptatum nam! Facilis laborum placeat eligendi facere alias?</p>
 </section>
 
 <style>
---
+++ src/routes/loading-messages.js
@@ -0,0 +1,8 @@
+export const messages = [
+	"reticulating splines...",
+	"Front End Web Development",
+	"salt air",
+	"Full Stack Web Development",
+	"Web Design",
+	"Game Development",
+];
