e987190b8e84cf10535a6bdefe75e4e1e4cf59e3
Jake <jakeviado434@gmail.com>
2023-10-31 21:02:17 +0800

    test blog page

---
+++ (root)/_app/immutable/nodes/3.2a2bc29b.js
@@ -0,0 +1,14 @@
+import{s as d,n as a}from"../chunks/scheduler.19461008.js";import{S as h,i as f,g as u,s as b,A as g,h as m,f as n,c as v,y as _,k as r,z as q,a as l}from"../chunks/index.de6ea330.js";const x=!1,O=x,y=O,M=!0,L=Object.freeze(Object.defineProperty({__proto__:null,csr:y,prerender:M},Symbol.toStringTag,{value:"Module"}));function A(p){let e,o,t,c=`<h2>What the heck is this?</h2> <p>This website is made with <a href="https://kit.svelte.dev">Svelte</a></p> <p>I heckin&#39; love my college life because of fuck&#39;n happenings in my life
+    lately - October 30, 2023</p> <p>test lang HAHAHAHA - October 30, 2023</p> <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
+    consectetur, laboriosam quam iste in quaerat delectus rerum exercitationem
+    eos, laborum provident odit quos perferendis est unde rem natus eligendi.
+    Ipsum. - October 30, 2023</p> <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
+    consectetur, laboriosam quam iste in quaerat delectus rerum exercitationem
+    eos, laborum provident odit quos perferendis est unde rem natus eligendi.
+    Ipsum. - October 30, 2023</p> <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
+    consectetur, laboriosam quam iste in quaerat delectus rerum exercitationem
+    eos, laborum provident odit quos perferendis est unde rem natus eligendi.
+    Ipsum. - October 30, 2023</p> <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
+    consectetur, laboriosam quam iste in quaerat delectus rerum exercitationem
+    eos, laborum provident odit quos perferendis est unde rem natus eligendi.
+    Ipsum. - October 30, 2023</p>`;return{c(){e=u("meta"),o=b(),t=u("section"),t.innerHTML=c,this.h()},l(s){const i=g("svelte-jpz01n",document.head);e=m(i,"META",{name:!0,content:!0}),i.forEach(n),o=v(s),t=m(s,"SECTION",{class:!0,"data-svelte-h":!0}),_(t)!=="svelte-1xm79sw"&&(t.innerHTML=c),this.h()},h(){document.title="About",r(e,"name","description"),r(e,"content","Blogs"),r(t,"class","section-contents")},m(s,i){q(document.head,e),l(s,o,i),l(s,t,i)},p:a,i:a,o:a,d(s){s&&(n(o),n(t)),n(e)}}}class S extends h{constructor(e){super(),f(this,e,null,A,d,{})}}export{S as component,L as universal};
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,13 +11,33 @@
   </p>
 
   <p>
-    I heckin' love my college life because of fuck'n happenings to my life
+    I heckin' love my college life because of fuck'n happenings in my life
     lately - October 30, 2023
   </p>
 
+  <p>test lang HAHAHAHA - October 30, 2023</p>
   <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium quaerat
-    doloremque quasi? Nihil voluptatem esse cupiditate ipsum atque temporibus
-    modi dolores mollitia unde iusto. Non commodi eligendi impedit ullam ipsa.
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
+    consectetur, laboriosam quam iste in quaerat delectus rerum exercitationem
+    eos, laborum provident odit quos perferendis est unde rem natus eligendi.
+    Ipsum. - October 30, 2023
+  </p>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
+    consectetur, laboriosam quam iste in quaerat delectus rerum exercitationem
+    eos, laborum provident odit quos perferendis est unde rem natus eligendi.
+    Ipsum. - October 30, 2023
+  </p>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
+    consectetur, laboriosam quam iste in quaerat delectus rerum exercitationem
+    eos, laborum provident odit quos perferendis est unde rem natus eligendi.
+    Ipsum. - October 30, 2023
+  </p>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
+    consectetur, laboriosam quam iste in quaerat delectus rerum exercitationem
+    eos, laborum provident odit quos perferendis est unde rem natus eligendi.
+    Ipsum. - October 30, 2023
   </p>
 </section>
